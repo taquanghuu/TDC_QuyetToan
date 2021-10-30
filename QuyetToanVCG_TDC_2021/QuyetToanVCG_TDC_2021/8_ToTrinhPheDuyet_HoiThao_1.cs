be754267e137a7de306a2da3b539461473684aaf
huvcgmaycoquan <taquanghuuvcg@gmail.com>
2021-10-30 16:02:50 +0700

    c

--- QuyetToanVCG_TDC_2021/QuyetToanVCG_TDC_2021/8_ToTrinhPheDuyet_HoiThao_1.cs
+++ QuyetToanVCG_TDC_2021/QuyetToanVCG_TDC_2021/8_ToTrinhPheDuyet_HoiThao_1.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using DevExpress.XtraReports.UI;
+using System.Data;
 
 namespace QuyetToanVCG_TDC_2021
 {
@@ -20,6 +21,15 @@ namespace QuyetToanVCG_TDC_2021
 
         private void ReportHeader_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
         {
+            clsTbhopdong cls = new QuyetToanVCG_TDC_2021.clsTbhopdong();
+            cls.iId_hopdong = mid_hopdong___;
+            DataTable dt = cls.SelectOne();
+            clsDaTa cls2 = new QuyetToanVCG_TDC_2021.clsDaTa();
+            DataTable dt2 = cls2.tbNgayThang_SA_ID_HD_and_id_the_stt(mid_hopdong___, 6);
+            tentotrinh = "TỜ TRÌNH\nĐề nghị phê duyệt kế hoạch hội thảo thông qua quy trình\n" + cls.sTenhopdong.Value + "";
+            cancuhopdong= "         Căn cứ vào hợp đồng số " + cls.sSohopdong.Value + " ngày " + cls.daNgayhopdong.Value.ToString("dd/MM/yyyy") + " giữa Viện KT cơ giới quân sự và Cục TC-ĐL-CL về việc " + cls.sTenhopdong.Value.ToString() + "";
+            cancukehoac = "         Căn cứ kế hoạch và tiến độ thực hiện nhiệm vụ Kỹ thuật;\n         Phòng Đo lường - Thí nghiệm kính đề nghị Viện trưởng Viện KT cơ giới quân sự phê duyệt Kế hoạch Hội thảo thông qua quy trình "+cls.sTenhopdong.Value+"";
+
             pToTrinh.Value = tentotrinh;
             pCanCuHopDong.Value = cancuhopdong;
             pCanCuKeHoach.Value = cancukehoac;
